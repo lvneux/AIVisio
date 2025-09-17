6b19a893607904f7e6f8cc7331239c36d1cbb082
lvneux <104901260+lvneux@users.noreply.github.com>
2025-09-17 18:03:36 +0900

    Edit import

--- osc/__init__.py
+++ osc/__init__.py
@@ -2,12 +2,12 @@
 YouTube 영상 분석 및 세그먼트 추출 패키지
 """
 
-from .models import VideoSegment
-from .controllers.utils import time_str_to_seconds, seconds_to_time_str
-from .controllers.transcript import extract_transcript, save_transcript_to_file
-from .controllers.youtube_api import get_youtube_video_info, get_youtube_chapters, extract_chapters_from_description
-from .controllers.segments import segment_video_by_description, map_subtitles_to_segments
-from .controllers.file_io import save_segments_to_json, save_segments_to_txt, save_segments_with_subtitles_to_json
+from osc.models import VideoSegment
+from osc.controllers.utils import time_str_to_seconds, seconds_to_time_str
+from osc.controllers.transcript import extract_transcript, save_transcript_to_file
+from osc.controllers.youtube_api import get_youtube_video_info, get_youtube_chapters, extract_chapters_from_description
+from osc.controllers.segments import segment_video_by_description, map_subtitles_to_segments
+from osc.controllers.file_io import save_segments_to_json, save_segments_to_txt, save_segments_with_subtitles_to_json
 
 __all__ = [
     'VideoSegment',
