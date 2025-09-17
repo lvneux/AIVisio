0f51777c982d089b243455c1a71afca56259f029
lvneux <104901260+lvneux@users.noreply.github.com>
2025-09-17 18:04:02 +0900

    Edit import

--- osc/controllers/__init__.py
+++ osc/controllers/__init__.py
@@ -2,11 +2,11 @@
 Controllers package for OSC analysis
 """
 
-from .youtube_api import get_youtube_chapters, get_youtube_video_info
-from .transcript import extract_transcript
-from .segments import segment_video_by_description, map_subtitles_to_segments
-from .file_io import save_segments_to_json, save_segments_to_txt, save_segments_with_subtitles_to_json
-from .summary import generate_summary, batch_generate_summaries
+from osc.controllers.youtube_api import get_youtube_chapters, get_youtube_video_info
+from osc.controllers.transcript import extract_transcript
+from osc.controllers.segments import segment_video_by_description, map_subtitles_to_segments
+from osc.controllers.file_io import save_segments_to_json, save_segments_to_txt, save_segments_with_subtitles_to_json
+from osc.controllers.summary import generate_summary, batch_generate_summaries
 
 __all__ = [
     'get_youtube_chapters',
